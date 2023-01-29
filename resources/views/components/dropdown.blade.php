358c0a10d5482860300702fdf48433c68e53d45c
toka <tokahassan692@gmail.com>
2023-01-29 04:51:42 +0200

    first commit

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -8,7 +8,7 @@
     </div>
 
     {{-- Links --}}
-    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-20" style="display: none" >
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-20 overflow-auto max-h-52" style="display: none" >
 
         {{$slot}}
 
